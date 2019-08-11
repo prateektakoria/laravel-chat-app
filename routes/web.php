d9bf650aa5b669e450bcb9807670d82732154595
Prateek Saini <prateektakoria@gmail.com>
2019-08-11 13:32:02 +0530

    Adding Authentication and chat UI

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/userLogout', function(){
+	Auth::logout();
+	return back();
 });
